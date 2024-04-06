d6760d9d240837f5a95d55e6fd605661b0c7f3eb
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-04-06 18:50:04 +0200

    create navbar

---
+++ components/navbar/categories.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { categories } from "@/data/categories"
+import { Container } from "../container"
+import { CategoryBox } from "../category-box"
+import { usePathname, useSearchParams } from "next/navigation"
+
+export const Categories = () => {
+  const params = useSearchParams()
+  const category = params?.get("category")
+  const pathname = usePathname()
+  const isMainPage = pathname === "/"
+
+  if (!isMainPage) return null
+
+  return (
+    <>
+      <Container>
+        <div
+          className="
+          pt-4
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+          overflow-x-auto
+        "
+        >
+          {categories.map((item) => (
+            <CategoryBox
+              key={item.label}
+              label={item.label}
+              icon={item.icon}
+              selected={category === item.label}
+            />
+          ))}
+        </div>
+      </Container>
+    </>
+  )
+}
---
+++ components/navbar/index.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { SafeUser } from "@/types"
+
+import { Container } from "../container"
+import { Logo } from "./logo"
+import { Categories } from "./categories"
+import { Search } from "./search"
+import { UserMenu } from "./user-menu"
+
+interface NavbarProps {
+  currentUser?: SafeUser | null
+}
+
+export const Navbar = ({ currentUser }: NavbarProps) => {
+  // Add some functions here...
+
+  return (
+    <>
+      <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div className=" py-4 border-b-[1px]">
+          <Container>
+            <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+              <Logo />
+              <Search />
+              <UserMenu />
+            </div>
+          </Container>
+        </div>
+
+        <Categories />
+      </div>
+    </>
+  )
+}

61e48f3941dcb61ad11d92a04fed28a8fd9e17af
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-04-07 20:25:39 +0200

    create rent step 2: location

--- components/modals/rent-modal.tsx
+++ components/modals/rent-modal.tsx
@@ -6,10 +6,13 @@ import { useRentModal } from "@/store/use-rent-modal"
 import { categories } from "@/data/categories"
 
 import { useMemo, useState } from "react"
+import dynamic from "next/dynamic"
+
 import { Modal } from "./modal"
 import { Heading } from "../heading"
 import { Input } from "../inputs/input"
 import { CategoryInput } from "../inputs/category-input"
+import { CountrySelect } from "../inputs/country-select"
 
 enum STEPS {
   CATEGORY = 0,
@@ -54,6 +57,14 @@ export const RentModal = () => {
   const bathroomCount = watch("bathroomCount")
   const imageSrc = watch("imageSrc")
 
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../map"), {
+        ssr: false,
+      }),
+    [location]
+  )
+
   const setCustomValue = (id: string, value: any) =>
     setValue(id, value, {
       shouldDirty: true,
@@ -68,7 +79,6 @@ export const RentModal = () => {
     if (step !== STEPS.PRICE) return onNext()
 
     setIsLoading(true)
-
   }
 
   // const onSubmit: SubmitHandler<FieldValues> = (data) => {
@@ -134,128 +144,128 @@ export const RentModal = () => {
     </div>
   )
 
-  // if (step === STEPS.LOCATION) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Where is your place located?"
-  //         subtitle="Help guests find you!"
-  //       />
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
 
-  //       <CountrySelect
-  //         value={location}
-  //         onChange={(value) => setCustomValue('location', value)}
-  //       />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
 
-  //       <Map center={location?.latlng} />
-  //     </div>
-  //   );
-  // }
+        <Map center={location?.latlng} />
+      </div>
+    )
+  }
 
-  // if (step === STEPS.INFO) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Share some basics about your place"
-  //         subtitle="What amenitis do you have?"
-  //       />
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenitis do you have?"
+        />
 
-  //       <Counter
-  //         onChange={(value) => setCustomValue('guestCount', value)}
-  //         value={guestCount}
-  //         title="Guests"
-  //         subtitle="How many guests do you allow?"
-  //       />
+        {/* <Counter
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title="Guests"
+          subtitle="How many guests do you allow?"
+        />
 
-  //       <hr />
+        <hr />
 
-  //       <Counter
-  //         onChange={(value) => setCustomValue('roomCount', value)}
-  //         value={roomCount}
-  //         title="Rooms"
-  //         subtitle="How many rooms do you have?"
-  //       />
+        <Counter
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+        />
 
-  //       <hr />
+        <hr />
 
-  //       <Counter
-  //         onChange={(value) => setCustomValue('bathroomCount', value)}
-  //         value={bathroomCount}
-  //         title="Bathrooms"
-  //         subtitle="How many bathrooms do you have?"
-  //       />
-  //     </div>
-  //   )
-  // }
+        <Counter
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+        /> */}
+      </div>
+    )
+  }
 
-  // if (step === STEPS.IMAGES) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Add a photo of your place"
-  //         subtitle="Show guests what your place looks like!"
-  //       />
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
 
-  //       {/* <ImageUpload
-  //         onChange={(value) => setCustomValue('imageSrc', value)}
-  //         value={imageSrc}
-  //       /> */}
-  //     </div>
-  //   )
-  // }
+        {/* <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        /> */}
+      </div>
+    )
+  }
 
-  // if (step === STEPS.DESCRIPTION) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="How would you describe your place?"
-  //         subtitle="Short and sweet works best!"
-  //       />
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
 
-  //       <Input
-  //         id="title"
-  //         label="Title"
-  //         disabled={isLoading}
-  //         register={register}
-  //         errors={errors}
-  //         required
-  //       />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
 
-  //       <hr />
+        <hr />
 
-  //       <Input
-  //         id="description"
-  //         label="Description"
-  //         disabled={isLoading}
-  //         register={register}
-  //         errors={errors}
-  //         required
-  //       />
-  //     </div>
-  //   )
-  // }
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
 
-  // if (step === STEPS.PRICE) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Now, set your price"
-  //         subtitle="How much do you charge per night?"
-  //       />
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
 
-  //       <Input
-  //         id="price"
-  //         label="Price"
-  //         formatPrice
-  //         type="number"
-  //         disabled={isLoading}
-  //         register={register}
-  //         errors={errors}
-  //         required
-  //       />
-  //     </div>
-  //   )
-  // }
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          type="number"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
 
   return (
     <>
